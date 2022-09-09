387004fea5026c49cf2125dbc68dc9b314e838c0
Gerald <gaguocha@yahoo.co.uk>
2022-09-09 16:44:03 -0700

    Comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
-
 /**
- * main-entry point
- *
- * Description: prints all possible combinations of two two-digit numbers
- *
+ * main-progrtam entry point
  * Return:0 success, non zero fail
  */
-
 int main(void)
 {
-	int i, j;
+	int m, i, j;
 
-	for (i = 0; i < 99; i++)
+	for (i = '0'; i <='9'; i++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (j ='0'; j<='9'; j++)
 		{
-			putchar('0' + i / 10);
-			putchar('0' + i % 10);
-
-			putchar(' ');
-
-			putchar('0' + j / 10);
-			putchar('0' + j % 10);
-
-			if (i == 98 && j == 99)
-				break;
-			putchar(',');
-			putchar(' ');
+			for (m = '0'; m <= '9'; m++)
+			{
+				if (i < j && J < m)
+				{
+					putchar (i);
+					putchar (j);
+					putchar (m);
+					if (i == '7' && j == '8' && m == '9')
+					{
+						break;
+					}
+					else
+					{
+						putchar (',');
+						putchar(' ');
+					}
+				}
+			}
 		}
 	}
-	putchar('\n');
-	return (0);
+	putchar ('/n');
+				return (0);
 }
