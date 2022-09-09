9ca8d08e3697270d62621050b3f4140418d685e6
Gerald <gaguocha@yahoo.co.uk>
2022-09-09 16:09:25 -0700

    COmb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *  main-program entry point
+ */
+int main(void)
+{
+	int m, i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (m = '0'; m <= '9'; m++)
+			{
+				putchar(i);
+				putchar(j);
+				putcha(m);
+				if (i == '7' && j =='8' && m == '9')
+				{
+					break
+				}
+				else
+				{
+					putchar (',');
+					putchar(',');
+				}
+			}
+		}
+	}
+	putchar ('/n');
+				return (0);
+}
