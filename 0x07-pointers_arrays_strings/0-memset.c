ad5eacbf684fbcef858e926397a0779954aed148
Gerald <gaguocha@yahoo.co.uk>
2022-09-27 15:17:54 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,3 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
  * _memset - a function that fills
  *         memory with a constant byte
@@ -20,16 +17,16 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-/**
- * declare an unsigned int
- * because we are storing a
- * value that will always be
- * non-negative (zero or positive)
- */
+	/**
+	 * declare an unsigned int
+	 * because we are storing a
+	 * value that will always be
+	 * non-negative (zero or positive)
+	 */
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
 
 	return (s);
-}
+
