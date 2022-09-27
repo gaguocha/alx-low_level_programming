6f75ea34d375dfb6f92492ead68fb3e95594f203
Gerald <gaguocha@yahoo.co.uk>
2022-09-27 14:26:59 -0700

    TAsk o

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -26,10 +26,10 @@ char *_memset(char *s, char b, unsigned int n)
  * value that will always be
  * non-negative (zero or positive)
  */
-unsigned int i;
+	unsigned int i;
 
-for (i = 0; i < n; i++)
-s[i] = b;
+	for (i = 0; i < n; i++)
+		s[i] = b;
 
-return (s);
+	return (s);
 }
